019d4d8fc19d20d701853fdd040aff1c96c4afa8
meenakshiwrdsb <meenakshi_durairaj@wrdsb.ca>
2025-10-15 11:21:31 -0400

    script for insert query in the table hd_empinquiry_user

---
+++ scipts/insert_query_hd_empinquiry_user.sql
@@ -0,0 +1,13 @@
+SELECT TOP (1000) [employee_id]
+,[userid]
+,[admin]
+FROM [HDHRP].[dbo].[hd_empinquiry_user]
+
+
+insert into [hd_empinquiry_user] values('25342','DukeR',1)
+insert into [hd_empinquiry_user] values('37574','RAJUV',1)
+insert into [hd_empinquiry_user] values('41405','MENONP',1)  
+insert into [hd_empinquiry_user] values('36333','TOMCHIM',1)
+insert into [hd_empinquiry_user] values('48785','durairm',1)
+  
+  
