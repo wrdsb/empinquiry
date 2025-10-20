39669f52dae677a3db62e00b5ae857cd263fee17
meenakshiwrdsb <meenakshi_durairaj@wrdsb.ca>
2025-10-20 15:09:14 -0400

    script chnage

--- scipts/insert_query_hd_empinquiry_user.sql
+++ scipts/insert_query_hd_empinquiry_user.sql
@@ -3,11 +3,14 @@ SELECT TOP (1000) [employee_id]
 ,[admin]
 FROM [HDHRP].[dbo].[hd_empinquiry_user]
 
+/* Authorized person records */
 
 insert into [hd_empinquiry_user] values('25342','DukeR',1)
 insert into [hd_empinquiry_user] values('37574','RAJUV',1)
 insert into [hd_empinquiry_user] values('41405','MENONP',1)  
 insert into [hd_empinquiry_user] values('36333','TOMCHIM',1)
 insert into [hd_empinquiry_user] values('48785','durairm',1)
+insert into [hd_empinquiry_user] values('26077','SANDERJ', 1)
+insert into [hd_empinquiry_user] values('27675','marshad', 1)
   
   
