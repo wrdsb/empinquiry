5ce4fd9629fecd48cf65a1ddb5cd68bffd7e5407
meenakshiwrdsb <meenakshi_durairaj@wrdsb.ca>
2025-11-17 13:52:22 -0500

    Authorized more team members

--- scipts/insert_query_hd_empinquiry_user.sql
+++ scipts/insert_query_hd_empinquiry_user.sql
@@ -1,7 +1,7 @@
 SELECT TOP (1000) [employee_id]
 ,[userid]
 ,[admin]
-FROM [HDHRP].[dbo].[hd_empinquiry_user]
+FROM [HDHRP].[dbo].[hd_empinquiry_user] 
 
 /* Authorized person records */
 
@@ -12,5 +12,17 @@ insert into [hd_empinquiry_user] values('36333','TOMCHIM',1)
 insert into [hd_empinquiry_user] values('48785','durairm',1)
 insert into [hd_empinquiry_user] values('26077','SANDERJ', 1)
 insert into [hd_empinquiry_user] values('27675','marshad', 1)
+/* Priya's Team Members Records */
+insert into [hd_empinquiry_user] values('33313','BELISLE', 1)
+insert into [hd_empinquiry_user] values('31299','PEGGMA', 1)
+insert into [hd_empinquiry_user] values('31663','RAYMONC', 1)
+insert into [hd_empinquiry_user] values('46367','PARVEZU', 1)
+insert into [hd_empinquiry_user] values('48305','NAKSHAN', 1)
+insert into [hd_empinquiry_user] values('28814','WolfeB', 1)
+
+
+
+
+
   
   
