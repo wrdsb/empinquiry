d7b8c657be41f9f02bb7ed62235055ca89c4eacb
meenakshiwrdsb <meenakshi_durairaj@wrdsb.ca>
2026-06-05 15:16:52 -0400

    Add hd_empinquiry_smartphone table creation script

---
+++ scripts/sql/create-objects/tables/create_table_hd_empinquiry_smartphone.sql
@@ -0,0 +1,38 @@
+USE [HDHRP]
+GO
+
+/*
+Author:		Meenakshi Durairaj 
+Date:		2026-JUN-05
+Purpose:	Table to store smartphone orders of the employee
+*/
+
+
+SET ANSI_NULLS ON
+GO
+
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[hd_empinquiry_smartphone]') AND type in (N'U'))
+DROP TABLE [dbo].[hd_empinquiry_smartphone]
+GO
+
+
+
+CREATE TABLE hd_empinquiry_smartphone
+(   
+      employee_id VARCHAR(9) NOT NULL    
+    , employee_name VARCHAR(90) NOT NULL
+    , order_date DATE NULL
+    , phone_number VARCHAR(50) NULL
+    , tier VARCHAR(10) NULL   -- Tier1/Tier2/Tier3/Tier4
+    , ordered_item VARCHAR(20) NULL  -- Sim / Phone
+    , rogers_account_created BIT NULL
+    , board_contribution_paid BIT NULL
+    , next_eligible_date DATE NULL
+    , form_link VARCHAR(1000) NULL
+    , notes VARCHAR(MAX) NULL
+    , created_date DATETIME DEFAULT GETDATE()
+    , Id INT IDENTITY(1,1) PRIMARY KEY
+);
+
+GO
